5c94237434567dc5253759a5d091306704c35f8e
SuruthiRaju <suruthi25@gmail.com>
2018-10-10 13:28:25 -0400

    added books

--- sql/insert_books.sql
+++ sql/insert_books.sql
@@ -6,9 +6,9 @@ VALUES
 ('The Intelligent Investor: The Definitive Book on Value Investing','Benjamin Graham','Paperback',640,'Harper Business; REV edition',2006,'English','9780060555665','978-0060555665'),
 ('Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!','Robert T. Kiyosaki','Paperback',336,'Plata Publishing; Second edition',2017,'English','1612680194','978-1612680194'),
 ('The Elements of Pizza: Unlocking the Secrets to World-Class Pies at Home','Ken Forkish','Hardcover',256,'Ten Speed Press',2016,'English','160774838X','978-1607748380'),
-('The five Element', 'Razine Bens', 459, 'Za Publish', '2018', 'English', '0345803123','978-0062641512'),
-('The Big MAC', 'Mark BigMac', 233, 'FoodAndBooks', '2017', 'German, English', '0140337396', '978-0011641540'),
-('The Big Three','Jeremy Steinfield', 599, 'MarketPub', '2016', 'Arabic, English', '0345803499', '978-0166677398');
+('Eyewitness Travel Guides: 2019.','Bostrom, Annie',140,'Book list edition',2019,'English','9781111112','978-9781111112'),
+('Educated: A Memoir','Tara Westover',493,'Random House Large Print',2018,'English','1635461855','978-1635461855'),
+('The Mars Room','Simon and Schuster',256,'Test Example Press',2018,'English','1982100753','978-1982100753');
 
 
 INSERT INTO musics (type, title, release_date, artist, label, asin)

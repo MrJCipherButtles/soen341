4c5126309ba2befc892964dbaa058f050e3a3039
SuruthiRaju <suruthi25@gmail.com>
2018-10-10 12:15:46 -0400

    added music

--- sql/insert_books.sql
+++ sql/insert_books.sql
@@ -6,3 +6,10 @@ VALUES
 ('The Intelligent Investor: The Definitive Book on Value Investing','Benjamin Graham','Paperback',640,'Harper Business; REV edition',2006,'English','9780060555665','978-0060555665'),
 ('Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!','Robert T. Kiyosaki','Paperback',336,'Plata Publishing; Second edition',2017,'English','1612680194','978-1612680194'),
 ('The Elements of Pizza: Unlocking the Secrets to World-Class Pies at Home','Ken Forkish','Hardcover',256,'Ten Speed Press',2016,'English','160774838X','978-1607748380');
+
+
+INSERT INTO musics (type, title, release_date, artist, label, asin)
+VALUES 
+('CD','Stormy Monday','1990-12-12','The Allman Brothers Band','At Fillmore East','1234567890'),
+('CD','The Chipmunk Song','1959-12-12','Ross Bagdasarian Sr.','Liberty','3456789012'),
+('CD','Green Christmas','1958-12-02','Stan Freberg','Capitol Records','2345678901') ;

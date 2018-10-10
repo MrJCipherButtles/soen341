11a51f4dfdf02ff5d2955a5adcddbe77986580b4
Your Name <rbensar1@ypg.com>
2018-10-10 12:21:41 -0400

    added records on movie table

--- sql/insert_movies.sql
+++ sql/insert_movies.sql
@@ -1,4 +1,6 @@
 INSERT INTO movies (title,director,producer,actors,languages,subtitles,dubbed,release_date,runtime)
 VALUES
-('Spider-Man', 'Sam Raimi', 'Laura Ziskin, Ian Bryce', 'Raz Bens, Suruthi Priya, Naimur Namur', 'English, French', 'Spanish, French, English, Arabic', 'English, French, German, Hindi', '2002-04-29', '121');
+('Spider-Man', 'Sam Raimi', 'Laura Ziskin, Ian Bryce', 'Raz Bens, Suruthi Priya, Naimur Namur', 'English, French', 'Spanish, French, English, Arabic', 'English, French, German, Hindi', '2002-04-29', '121'),
+('The super MAC', 'Raz Booboo', 'Nicolas Samaha', 'Nocolas Samaha, Joshua Butler', 'Arabic, French, English', 'Arabic, French, English', 'Arabic, French, English', '2017-01-01', '129'),
+('The Big Fish', 'Rami Eldiaz', 'Barbara Samaha', 'Nocolas Saluti, Josh Aller', 'French, English', 'French, English', 'Arabic, French, English, Spanish, Mandarin', '2006-05-01', '139');
 

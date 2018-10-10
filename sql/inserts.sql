86931ed31b4eaef7bde57d9fda17a22ee0fb5d3c
Naimur Rashid <RashidN96@gmail.com>
2018-10-10 13:14:04 -0400

    added 3 books

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -5,7 +5,10 @@ VALUES
 ('Of Mice and Men','John Steinbeck',112,'Penguin Books',1993,'English','0140177396','978-0140177398'),
 ('The Intelligent Investor: The Definitive Book on Value Investing','Benjamin Graham','Paperback',640,'Harper Business; REV edition',2006,'English','9780060555665','978-0060555665'),
 ('Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!','Robert T. Kiyosaki','Paperback',336,'Plata Publishing; Second edition',2017,'English','1612680194','978-1612680194'),
-('The Elements of Pizza: Unlocking the Secrets to World-Class Pies at Home','Ken Forkish','Hardcover',256,'Ten Speed Press',2016,'English','160774838X','978-1607748380');
+('The Elements of Pizza: Unlocking the Secrets to World-Class Pies at Home','Ken Forkish','Hardcover',256,'Ten Speed Press',2016,'English','160774838X','978-1607748380'),
+('The Giver','Lois Lowry',240,'HMH Books for Young Readers',2014,'English', '0544336267','978-0544336261'),
+('Leonis','Mario Francis',300,'Les Intouchables ',2004,'French', '2895491380','978-2895491385'),
+('Cravings: Hungry for More','Chrissy Teigen',256,'Clarkson Potter',2018,'English', '1524759724','978-1524759728');
 
 INSERT INTO magazines (title,publisher,year_published,language,isbn_10,isbn_13)
 VALUES

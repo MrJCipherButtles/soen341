c4e29f5065db748997ca6010c31d192c5db7f005
MrJCipherButtles <butler.joshua355@gmail.com>
2018-10-10 12:29:10 -0400

    Created file to be used for all INSERT statements

---
+++ sql/inserts.sql
@@ -0,0 +1,20 @@
+INSERT INTO books (title,author,num_pages,publisher,year_published,language,isbn_10,isbn_13)
+VALUES 
+('Fifty Shades of Grey','E. L. James',514,'Vintage Books',2011,'English', '0345803485','978-0345803481'),
+('The Subtle Art of Not Giving a F*ck','Mark Manson',224,'Harper',2016,'English','0062641549','978-0062641540'),
+('Of Mice and Men','John Steinbeck',112,'Penguin Books',1993,'English','0140177396','978-0140177398'),
+('The Intelligent Investor: The Definitive Book on Value Investing','Benjamin Graham','Paperback',640,'Harper Business; REV edition',2006,'English','9780060555665','978-0060555665'),
+('Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!','Robert T. Kiyosaki','Paperback',336,'Plata Publishing; Second edition',2017,'English','1612680194','978-1612680194'),
+('The Elements of Pizza: Unlocking the Secrets to World-Class Pies at Home','Ken Forkish','Hardcover',256,'Ten Speed Press',2016,'English','160774838X','978-1607748380');
+
+INSERT INTO magazines (title,publisher,year_published,language,isbn_10,isbn_13)
+VALUES
+();
+
+INSERT INTO musics (type,title,release_date,artist,label,asin)
+VALUES
+();
+
+INSERT INTO clients (firstName,lastName,addresse,email,phone,isAdmin)
+VALUES
+();

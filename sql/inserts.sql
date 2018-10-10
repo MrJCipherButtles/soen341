009afe3cd9755cee667fb13e57dc8ee8585a7d41
MrJCipherButtles <butler.joshua355@gmail.com>
2018-10-10 12:49:37 -0400

    copied records to inserts.sql

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -6,15 +6,44 @@ VALUES
 ('The Intelligent Investor: The Definitive Book on Value Investing','Benjamin Graham','Paperback',640,'Harper Business; REV edition',2006,'English','9780060555665','978-0060555665'),
 ('Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!','Robert T. Kiyosaki','Paperback',336,'Plata Publishing; Second edition',2017,'English','1612680194','978-1612680194'),
 ('The Elements of Pizza: Unlocking the Secrets to World-Class Pies at Home','Ken Forkish','Hardcover',256,'Ten Speed Press',2016,'English','160774838X','978-1607748380');
+('The five Element', 'Razine Bens', 459, 'Za Publish', '2018', 'English', '0345803123','978-0062641512'),
+('The Big MAC', 'Mark BigMac', 233, 'FoodAndBooks', '2017', 'German, English', '0140337396', '978-0011641540'),
+('The Big Three','Jeremy Steinfield', 599, 'MarketPub', '2016', 'Arabic, English', '0345803499', '978-0166677398'),
+('Grapes of Wrath', 'John Steinbeck', 241, 'Penguin Publishing', 1931, 'English',7593648531,'834-7593648531'),
+('Le monde sans Faim', 'George Lecavalier', 53, 'Access Litterature', 1994, 'French', 9301756562, '123-9301756562'),
+('People are Animals', 'Jane Joyce', 347, 'Worldwide', 2008, 'English', 9044591234, '892-9044591234');
 
 INSERT INTO magazines (title,publisher,year_published,language,isbn_10,isbn_13)
 VALUES
-();
+('Design News','UBM','1946','English','9000119407','987-9000119407'),
+('Bon Appétit','Craig Kostelic','1956','French','9000066990','987-9000066990'),
+('House Beautiful','Hearst Magazines','1996','English','9000186422','987-9000186422');
 
 INSERT INTO musics (type,title,release_date,artist,label,asin)
 VALUES
-();
+('CD','Stormy Monday','1990-12-12','The Allman Brothers Band','At Fillmore East','1234567890'),
+('CD','The Chipmunk Song','1959-12-12','Ross Bagdasarian Sr.','Liberty','3456789012'),
+('CD','Green Christmas','1958-12-02','Stan Freberg','Capitol Records','2345678901') ,
+('CD','Bangarang','2011-12-23','Skrillex','Big Beat, Atlantic, OWSLA','B005FLX1HS'),
+('Vinyl','Another One Bites The Dust','1980-12-21','Queen','EMI','B00EX6LCNG'),
+('CD','Starlight','2006-09-04','Muse','Warner Bros.','B002CNUSFK'),
+('CD', 'Wake me up in September', '2001-04-02', 'Lincooln Park', 'DC-Label', '2345633345'),
+('CD', 'I am ZA Best', '2018-03-03', 'Rassiol Bambini', 'Xtreme Muzik', '2323498901'),
+('CD', 'Hello Good Morning', '1976-03-27', 'Gorgio Lancini', 'Fedrolia', '1905678901'),
+('Vinyl','House of Balloons','2011-03-21','The Weeknd','XO','B011IOGYL0'),
+('CD','Thursday','2011-08-18','The Weeknd','XO','B010NUQNGQ'),
+('Digital','Echoes of Silence','2011-12-22','The Weeknd','Universal Republic Records','B00TINRK9E');
+
+
+INSERT INTO movies (title,director,producer,actors,languages,subtitles,dubbed,release_date,runtime)
+VALUES
+('Spider-Man', 'Sam Raimi', 'Laura Ziskin, Ian Bryce', 'Raz Bens, Suruthi Priya, Naimur Namur', 'English, French', 'Spanish, French, English, Arabic', 'English, French, German, Hindi', '2002-04-29', '121'),
+('The super MAC', 'Raz Booboo', 'Nicolas Samaha', 'Nocolas Samaha, Joshua Butler', 'Arabic, French, English', 'Arabic, French, English', 'Arabic, French, English', '2017-01-01', '129'),
+('The Big Fish', 'Rami Eldiaz', 'Barbara Samaha', 'Nocolas Saluti, Josh Aller', 'French, English', 'French, English', 'Arabic, French, English, Spanish, Mandarin', '2006-05-01', '139');
+
 
 INSERT INTO clients (firstName,lastName,addresse,email,phone,isAdmin)
 VALUES
-();
+('Justin', 'Malone', '950 real street, Montreal', 'justind.malone@domain.ca','514-514-5145', 'FALSE'),
+('Alex', 'Baker', '42 Baker street, Sherbrooke', 'alex.baker1997@domain.ca','514-514-5165', 'FALSE'),
+('Alfred', 'Augustus', '5716 Summer boulevard, Quebec City', 'AA@domain.ca','418-385-3893', 'FALSE');

63d40e30ab3eb74f6abeaf1158eb2ea91b8a6473
Your Name <rbensar1@ypg.com>
2018-10-10 12:09:58 -0400

    added file for insert movie and one record as example

---
+++ sql/insert_movies.sql
@@ -0,0 +1,4 @@
+INSERT INTO movies (title,director,producer,actors,languages,subtitles,dubbed,release_date,runtime)
+VALUES
+('Spider-Man', 'Sam Raimi', 'Laura Ziskin, Ian Bryce', 'Raz Bens, Suruthi Priya, Naimur Namur', 'English, French', 'Spanish, French, English, Arabic', 'English, French, German, Hindi', '2002-04-29', '121');
+

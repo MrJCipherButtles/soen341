9c97bbfdda08dcb1d757f195eaf012fbc90c9da2
Vincent Cerri <vincentcerri@Vincents-MacBook-Pro.local>
2018-10-10 12:40:46 -0400

    added 3 movies

--- sql/insert_movies.sql
+++ sql/insert_movies.sql
@@ -2,5 +2,7 @@ INSERT INTO movies (title,director,producer,actors,languages,subtitles,dubbed,re
 VALUES
 ('Spider-Man', 'Sam Raimi', 'Laura Ziskin, Ian Bryce', 'Raz Bens, Suruthi Priya, Naimur Namur', 'English, French', 'Spanish, French, English, Arabic', 'English, French, German, Hindi', '2002-04-29', '121'),
 ('The super MAC', 'Raz Booboo', 'Nicolas Samaha', 'Nocolas Samaha, Joshua Butler', 'Arabic, French, English', 'Arabic, French, English', 'Arabic, French, English', '2017-01-01', '129'),
-('The Big Fish', 'Rami Eldiaz', 'Barbara Samaha', 'Nocolas Saluti, Josh Aller', 'French, English', 'French, English', 'Arabic, French, English, Spanish, Mandarin', '2006-05-01', '139');
-
+('The Big Fish', 'Rami Eldiaz', 'Barbara Samaha', 'Nocolas Saluti, Josh Aller', 'French, English', 'French, English', 'Arabic, French, English, Spanish, Mandarin', '2006-05-01', '139'),
+('Ride Along 2','Tim Story','Will Packer, Ice Cube, Matt Alvarez, Larry Brezner','Kevin Hart, Ice Cube, Ken Jeong, Benjamin Bratt, Olivia Munn, Bruce McGill','English','English, French, Arabic, Chinese, Spanish','English, Spanish, French, Hindi, Russian','2016-01-15','102'),
+('Just Go With It','Dennis Dugan','Adam Sandler, Jack Giarraputo, Heather Parry','Adam Sandler, Jennifer Aniston, Nick Swardson, Brooklyn Decker, Dave Matthews, Bailee Madison, Nicole Kidman','English','English, French','English, French','2011-01-11','117'),
+('The Fate of the Furious','F. Gary Gray','Neal H. Moritz, Vin Diesel, Michael Fottrell, Chris Morgan','Vin Diesel, Dwayne Johnson, Jason Statham, Michelle Rodriguez, Tyrese Gibson, Chris "Ludacris" Bridges, Scott Eastwood, Nathalie Emmanuel, Elsa Pataky, Kurt Russell, Charlize Theron','English','English, French, Spanish','English, French','2017-04-14','136');

1fc7967ac49d072db3f038479c19582585efbe5b
MrJCipherButtles <butler.joshua355@gmail.com>
2018-10-10 23:04:40 -0400

    Fine tuned database settings

--- sql/library.sql
+++ sql/library.sql
@@ -3,24 +3,29 @@ This is SQL code for creating a library database
 on a MYSQL Server
 */
 
+SET GLOBAL validate_password_policy=LOW;
+SET GLOBAL validate_password_length=5;
+SET GLOBAL max_user_connections=1;
+
 CREATE DATABASE library;
 USE library;
 
 -- hardcoded admin
 
--- CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'; -- username = admin / password = admin
--- GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'; -- username = admin / password = admin
+GRANT ALL PRIVILEGES ON library.* TO 'admin'@'localhost'
+	WITH GRANT OPTION;
 
 -- entity tables
 
 CREATE TABLE books (
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	title VARCHAR(100),
-	author VARCHAR(100),
+	title VARCHAR(255),
+	author VARCHAR(255),
 	num_pages INT,
-	publisher VARCHAR(100),
+	publisher VARCHAR(255),
 	year_published YEAR(4),
-	language VARCHAR(100),
+	language VARCHAR(255),
 	isbn_10 CHAR(10),
 	isbn_13 CHAR(14),
 	PRIMARY KEY (id)
@@ -28,10 +33,10 @@ CREATE TABLE books (
 
 CREATE TABLE magazines (
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	title VARCHAR(100),
-	publisher VARCHAR(100),
+	title VARCHAR(255),
+	publisher VARCHAR(255),
 	year_published YEAR(4), -- MYSQL YEAR Type
-	language VARCHAR(100),
+	language VARCHAR(255),
 	isbn_10 CHAR(10),
 	isbn_13 CHAR(14),
 	PRIMARY KEY (id)
@@ -39,24 +44,24 @@ CREATE TABLE magazines (
 
 CREATE TABLE musics (
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	type VARCHAR(100),
-	title VARCHAR(100),
+	type VARCHAR(255),
+	title VARCHAR(255),
 	release_date DATE,
-	artist VARCHAR(100),
-	label VARCHAR(100),
+	artist VARCHAR(255),
+	label VARCHAR(255),
 	asin CHAR(10),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE movies (
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	title VARCHAR(100),
-	director VARCHAR(100),
-	producer VARCHAR(100),
+	title VARCHAR(255),
+	director VARCHAR(255),
+	producer VARCHAR(255),
 	actors TEXT,
-	languages VARCHAR(100),
-	subtitles VARCHAR(100),
-	dubbed VARCHAR(100),
+	languages VARCHAR(255),
+	subtitles VARCHAR(255),
+	dubbed VARCHAR(255),
 	release_date DATE,
 	runtime SMALLINT UNSIGNED, -- in minutes
 	PRIMARY KEY (id)
@@ -64,10 +69,10 @@ CREATE TABLE movies (
 
 CREATE TABLE clients (
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	firstName VARCHAR(100) NOT NULL,
-	lastName VARCHAR(100) NOT NULL,
-	address VARCHAR(100),
-	email VARCHAR(100),
+	firstName VARCHAR(255) NOT NULL,
+	lastName VARCHAR(255) NOT NULL,
+	address VARCHAR(255),
+	email VARCHAR(255),
 	phone VARCHAR(20),
 	isAdmin ENUM('TRUE','FALSE') NOT NULL DEFAULT 'FALSE',
 	PRIMARY KEY (id)

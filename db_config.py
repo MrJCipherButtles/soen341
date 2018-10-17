eef3bce53386f6253ed0f91a3fd1ad59ed0ea1dd
jcarrier <justind.carrier@gmail.com>
2018-10-17 11:38:00 -0400

    Added basic db validation

---
+++ db_config.py
@@ -0,0 +1,4 @@
+db_user = 'root'
+db_password = 'your pw'
+db_name = 'library'
+db_host = 'localhost'
--- front.py
+++ front.py
@@ -1,8 +1,21 @@
 from flask import Flask, Response, render_template, request
 import os.path
+from db_config import db_host, db_name, db_password, db_user
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = db_user
+app.config['MYSQL_DATABASE_PASSWORD'] = db_password
+app.config['MYSQL_DATABASE_DB'] = db_name
+app.config['MYSQL_DATABASE_HOST'] = db_host
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route("/login", methods=['GET', 'POST'])
 def hello():
@@ -11,7 +24,11 @@ def hello():
     else:
         fname = request.form['fname']
         lname = request.form['lname']
-        return fname + lname
+        cursor.execute("SELECT * FROM library.clients WHERE firstName = '%s' AND lastName = '%s'" % (fname, lname))
+        cursor.fetchall()
+        if cursor.rowcount == 0:
+            return "Does not exist"
+        return "login successful"
 
 
 

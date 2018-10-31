c55c68f223b15090b2aa493421ca70ee680fb854
jcarrier <justind.carrier@gmail.com>
2018-10-31 12:32:38 -0400

    Made View classes with static methods instead of just being files with methods

--- library.py
+++ library.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from db_connection import DBGateway
-from views.login import show_login_page, verify_login
-from views.register import register_user
+from views.login import Login
+from views.register import Register
 
 app = Flask(__name__)
 db_gateway = DBGateway(app)
@@ -10,9 +10,9 @@ db_gateway = DBGateway(app)
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return show_login_page()
+        return Login.show_login_page()
     else:
-        return verify_login(db_gateway, request)
+        return Login.verify_login(db_gateway, request)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -20,7 +20,7 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        return register_user(db_gateway, request)
+        return Register.register_user(db_gateway, request)
 
 
 if __name__ == "__main__":
--- views/login.py
+++ views/login.py
@@ -1,17 +1,19 @@
 from flask import render_template
 
 
-def verify_login(db_gateway, request):
-    fname = request.form['user']
-    lname = request.form['psw']
-    # TODO: Include raw sql inside db_gateway
-    db_gateway.cursor.execute(
-        "SELECT * FROM library.clients WHERE firstName = '%s' AND lastName = '%s'" % (fname, lname))
-    db_gateway.cursor.fetchall()
-    if db_gateway.cursor.rowcount == 0:
-        return "Does not exist"
-    return "login successful"
+class Login:
+    @staticmethod
+    def verify_login(db_gateway, request):
+        fname = request.form['user']
+        lname = request.form['psw']
+        # TODO: Include raw sql inside db_gateway
+        db_gateway.cursor.execute(
+            "SELECT * FROM library.clients WHERE firstName = '%s' AND lastName = '%s'" % (fname, lname))
+        db_gateway.cursor.fetchall()
+        if db_gateway.cursor.rowcount == 0:
+            return "Does not exist"
+        return "login successful"
 
-
-def show_login_page():
-    return render_template('login.html')
+    @staticmethod
+    def show_login_page():
+        return render_template('login.html')

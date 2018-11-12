ecce24df10a4976190ebee75705f069202f4a8a2
Naimur Rashid <RashidN96@gmail.com>
2018-11-11 19:05:39 -0500

    fixed date for loan

--- controller/loan.py
+++ controller/loan.py
@@ -1,4 +1,5 @@
 from flask import request
+import datetime
 
 class Loan:
     @staticmethod
@@ -10,6 +11,6 @@ class Loan:
 
         db_gateway.cursor.execute(
             "INSERT INTO library.loans (clientId, itemId, loan_date) VALUES ('%s', '%s', '%s')" % (
-                user, itemID, '2017-01-01'))
+                user, itemID, datetime.datetime.today().strftime('%Y-%m-%d')))
         db_gateway.conn.commit()
         return "loan successful"

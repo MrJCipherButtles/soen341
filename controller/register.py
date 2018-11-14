720049c5250603d480398f17920bcf6ffcca582e
Your Name <rbensar1@ypg.com>
2018-11-13 20:40:58 -0500

    added login redirect option to bypass nested views

--- controller/register.py
+++ controller/register.py
@@ -10,4 +10,6 @@ class Register:
     @staticmethod
     def register_user_admin(db_gateway, request):
         db_gateway.register_user_admin(request)
-        return redirect(url_for('login'))
+        return redirect(url_for('http://127.0.0.1:5000/login'))
+      #I redirect to the login page. Othrewise, the login page will be
+    # rendered within the navguaion bar and side bars which creates a nested view (bad design)
